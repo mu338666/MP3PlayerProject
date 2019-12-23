c855059c0c20bfefd4b49ded6a6104f9e0e3f400
mu338666 <58577791+mu338666@users.noreply.github.com>
2019-12-24 07:19:07 +0900

    MP3PlayerProject Git 연동

---
+++ app/src/main/java/com/example/mu338/mp3playerproject/ClickListener.java
@@ -0,0 +1,12 @@
+package com.example.mu338.mp3playerproject;
+
+import android.view.View;
+
+public interface ClickListener {
+
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+}
+
+
